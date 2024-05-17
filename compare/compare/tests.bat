afc7997c9a86ea65e270dbf862995d87af71881f
unknown <flord35@yandex.ru>
2024-05-17 11:05:15 +0300

    Переделаны тесты

--- compare/compare/tests.bat
+++ compare/compare/tests.bat
@@ -15,57 +15,62 @@ REM Начало тестов
 
 REM Запуск программы без аргументов
 %MyProgram% > %MyOut% && goto err
+fc %MyOut% InvalidArguments-out.txt > nul || goto err
 echo Test 1 passed
 
 REM Запуск программы с одним аргументом
 %MyProgram% AverageFile.txt > %MyOut% && goto err
+fc %MyOut% InvalidArguments-out.txt > nul || goto err
 echo Test 2 passed
 
 REM Запуск программы с тремя аргументами
 %MyProgram% AverageFile.txt AverageFile.txt AverageFile.txt > %MyOut% && goto err
+fc %MyOut% InvalidArguments-out.txt > nul || goto err
 echo Test 3 passed
 
 REM Сравнение пустого файл с самим собой
 %MyProgram% Empty.txt Empty.txt > %MyOut% || goto err
-fc Empty.txt Empty.txt > nul || goto err
+fc %MyOut% EqualFiles-out.txt > nul || goto err
 echo Test 4 passed
 
 REM Сравнение пустого файл с его копией
 %MyProgram% Empty.txt EmptyCopy.txt > %MyOut% || goto err
-fc Empty.txt EmptyCopy.txt > nul || goto err
+fc %MyOut% EqualFiles-out.txt > nul || goto err
 echo Test 5 passed
 
 REM Запуск программы с несуществующим файлом
 %MyProgram% Missing.txt AverageFile.txt > %MyOut% && goto err
+fc %MyOut% MissingFile-out.txt > nul || goto err
 echo Test 6 passed
 
 REM Сравнение несуществующего файла с самим собой
-%MyProgram% Missing.txt AverageFile.txt > %MyOut% && goto err
+%MyProgram% Missing.txt Missing.txt > %MyOut% && goto err
+fc %MyOut% MissingFile-out.txt > nul || goto err
 echo Test 7 passed
 
-REM Сравнение двух похожих файлов с отличием
+REM Сравнение двух похожих файлов с отличием (6 строка)
 %MyProgram% AverageFile.txt AverageFileWithDifferences.txt > %MyOut% && goto err
-fc /n AverageFile.txt AverageFileWithDifferences.txt && goto err
+fc %MyOut% DifferentFilesAtLineSix-out.txt > nul || goto err
 echo Test 8 passed
 
-REM Сравнение двух одинаковых файлов, но в одном из них грамматическая ошибка
+REM Сравнение двух одинаковых файлов, но в одном из них грамматическая ошибка (4 строка)
 %MyProgram% AverageFile.txt AverageFileWithMinorSpellingMistake.txt > %MyOut% && goto err
-fc /n AverageFile.txt AverageFileWithMinorSpellingMistake.txt && goto err
+fc %MyOut% DifferentFilesAtLineFour-out.txt > nul || goto err
 echo Test 9 passed
 
-REM Сравнение двух одинаковых файлов, но в одном из них лишний переход на новую строку в конце (Файлы, похоже, считаются равными)
+REM Сравнение двух одинаковых файлов, но в одном из них лишний переход на новую строку в конце (Файлы считаются равными)
 %MyProgram% AverageFile.txt AverageFileWithNewLineInTheEnd.txt > %MyOut% || goto err
-fc /n AverageFile.txt AverageFileWithNewLineInTheEnd.txt || goto err
+fc %MyOut% EqualFiles-out.txt > nul || goto err
 echo Test 10 passed
 
-REM Сравнение двух одинаковых файлов, но в одном из них лишний переход на новую строку в середине
+REM Сравнение двух одинаковых файлов, но в одном из них лишний переход на новую строку в середине (строка 3)
 %MyProgram% AverageFile.txt AverageFileWithNewLineInTheMiddle.txt > %MyOut% && goto err
-fc /n AverageFile.txt AverageFileWithNewLineInTheMiddle.txt && goto err
+fc %MyOut% DifferentFilesAtLineThree-out.txt > nul || goto err
 echo Test 11 passed
 
 REM Сравнение двух одинаковых файлов с особыми символами
 %MyProgram% AverageFile.txt AverageFileWithNewLineInTheEnd.txt > %MyOut% || goto err
-fc AverageFile.txt AverageFileWithNewLineInTheEnd.txt > nul || goto err
+fc %MyOut% EqualFiles-out.txt > nul || goto err
 echo Test 12 passed
 
 
